ad968a868f99f6f13ca6ad6f1d8fde2f0588f274
Will Schroeder <willjrschroeder@gmail.com>
2022-06-24 17:52:43 -0500

    completed exercises

---
+++ javascript/starter-sandbox/sandbox.js
@@ -0,0 +1,21 @@
+console.log("Hello, world");
+
+let y = 8;
+let x = 7;
+let z = 2;
+
+let val = (x - z) * y;
+
+let str = "Hello";
+
+console.log(val);
+console.log(str.substring(2, 5));
+console.log(str.substr(1, 1));
+console.log(str.slice(2, 5));
+
+console.log(str.replace("lo", "p"));
+
+let tester = "Hi there my name is Will"
+console.log(tester.replace(" ", "-"));
+
+console.log(tester.split(" ").join("-"));
